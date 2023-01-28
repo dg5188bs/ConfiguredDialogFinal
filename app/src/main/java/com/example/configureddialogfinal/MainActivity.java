0889d08a4fc9373cc99da489aa6f24741c8c0210
dg5188bs <dg5188@bs.amalnet.k12.il>
2023-01-28 20:33:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/configureddialogfinal/MainActivity.java
@@ -0,0 +1,132 @@
+package com.example.configureddialogfinal;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.TypeConverter;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Intent si;
+    AlertDialog.Builder adb,adb2,adb3;
+    final String[] colors ={"Red","Green","Blue"};
+    int color[];
+    LinearLayout linearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        linearLayout = (LinearLayout) findViewById(R.id.lin);
+        si = new Intent(this,MainActivity2.class);
+
+
+    }
+
+    public void first(View view) {
+        adb = new AlertDialog.Builder(this);
+        adb.setCancelable(false);
+        color = new int[]{0,0,0};
+        adb.setTitle("chose one color");
+        adb.setItems(colors, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                color[i] = 255;
+                linearLayout.setBackgroundColor(Color.rgb(color[0],color[1],color[2]));
+            }
+        });
+        adb.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        AlertDialog ad =adb.create();
+        ad.show();
+    }
+
+    public void second(View view) {
+        adb2 = new AlertDialog.Builder(this);
+        adb2.setCancelable(false);
+        color = new int[]{0,0,0};
+        adb2.setTitle("You can choose more than one color");
+        adb2.setMultiChoiceItems(colors, null, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                if(b) {
+                    color[i] = 255;
+                }
+                else if(color[i]==255){
+                    color[i] = 0;
+                }
+            }
+        });
+        adb2.setNegativeButton("Cencel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        adb2.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                linearLayout.setBackgroundColor(Color.rgb(color[0],color[1],color[2]));
+            }
+        });
+
+
+        AlertDialog ad =adb2.create();
+        ad.show();
+    }
+
+    public void Third(View view) {
+        linearLayout.setBackgroundColor(Color.WHITE);
+    }
+
+    public void fourth(View view) {
+        adb3 = new AlertDialog.Builder(this);
+        adb3.setCancelable(false);
+        adb3.setTitle("Type here to see magic");
+        final EditText eT = new EditText(this);
+        eT.setHint("Type text here");
+        adb3.setView(eT);
+        adb3.setNegativeButton("Cencel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        adb3.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String st = eT.getText().toString();
+                Toast.makeText(MainActivity.this,st,Toast.LENGTH_SHORT).show();
+            }
+        });
+        AlertDialog ad =adb3.create();
+        ad.show();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        String st = menuItem.getTitle().toString();
+        if(st.equals("credits")){
+            startActivity(si);
+
+
+        }
+        return true;
+    }
+}
